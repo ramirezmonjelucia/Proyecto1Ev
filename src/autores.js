b30afc4cdbb007cc1de6a6f29c813fb2660ab8f4
Lucia Ramirez Monje <lramirez@iespuntadelverde.es>
2020-11-22 20:01:56 +0100

    first commit

--- src/autores.js
+++ src/autores.js
@@ -5,16 +5,18 @@ PRIMERO habría que modificar la colección de los libros, y donde está el nomb
 SEGUNDO habría que crear una nueva coleccion llamada autores donde aparecen los datos de los autores así como su id, 
 que es el que se relacionará posteriormente a cada autor con su libro, mediante el idAutor
 */
-db.libros.insertOne(
+db.libros.updateMany({ $rename: { "Autor": "idAutor" } } )
+/*
     {Título: "Secuestrado",
     Editorial: "Alba Editorial",
-    idAutor: 1,
+    idAutor: ,
     Fecha: "19-09-2018",
     NºPáginas: 280,
     Dimensiones_mm: [210, 140],
     Géneros: ["Novela", "Ficción especulativa", "Novela histórica", "Aventuras"],
     Sinópsis: "A la muerte en 1751 de su padre, maestro rural, el joven David Balfour, aleccionado por el párroco del pueblo, que le entrega una carta del difunto para llevar en mano «al distinguido caballero Ebenezer Balfour de Shaws», emprende un viaje a casa de su tío con la perspectiva de mejorar su condición con una herencia inesperada. Pero su destino resulta ser una lóbrega mansión y el señor Ebenezer «un ser miserable, encorvado y estrecho de hombros, con una cara que parecía de arcilla». Con falsas promesas lo embarca en un bergantín, con la intención de venderle como esclavo. Narrada por su protagonista, con la elegante prosa de Robert Louis Stevenson, Secuestrado (1886) es una novela de padecimiento y recompensa."})
-
+*/
+db.libros.updateMany({ $rename: { "Autor": "idAutor" } } )
 
 db.autores.insertOne({
     idAutor: 1,
